fde3c8934a4e920b137d28200c445b64f5e852a0
Austin313 <32691123+Austin313@users.noreply.github.com>
2021-10-25 14:37:20 -0500

    Added Math operators assignment

---
+++ MathAndComparisonAssignment/MathAndComparisonAssignment/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+
+namespace MathAndComparisonAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hourlyRate1 = 15;
+            int hourlyRate2 = 20;
+            int hoursWorked = 40;
+            int salary1 = hourlyRate1 * hoursWorked * 52;
+            int salary2 = hourlyRate2 * hoursWorked * 52;
+            bool compare = salary1 > salary2;
+            
+            Console.WriteLine("Anonymous Income Comparison Program");
+            Console.WriteLine("Person 1");
+            Console.WriteLine("Hourly Wage?");
+            Console.WriteLine(hourlyRate1);
+            Console.WriteLine("hours worked?");
+            Console.WriteLine(hoursWorked);
+            Console.WriteLine("Person 2");
+            Console.WriteLine("Hourly Wage?");
+            Console.WriteLine(hourlyRate2);
+            Console.WriteLine("hours worked");
+            Console.WriteLine(hoursWorked);
+            Console.WriteLine("Annual Salary of Person 1:");
+            Console.WriteLine(salary1);
+            Console.WriteLine("Annual Salary of Person 2:");
+            Console.WriteLine(salary2);
+            Console.WriteLine("Does Person 1 make more than Person 2?");
+            Console.WriteLine(compare);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Operators/Operators/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int total = 5 + 10;
+            //int otherTotal = 12 + 22;
+            //int combined = total + otherTotal;
+            //Console.WriteLine(combined);
+            //Console.ReadLine();
+
+            //int difference = 10 - 5;
+            //Console.WriteLine("five minus ten = " + difference.ToString();
+            //Console.ReadLine();
+
+            //int product = 10 * 5;
+            //Console.WriteLine(product);
+            //Console.ReadLine();
+
+            //int qoutient = 100.0 / 17.0;
+            //Console.WriteLine(qoutient);
+            //Console.ReadLine();
+
+            //int remainder = 11 % 2;
+            //Console.WriteLine(remainder);
+            //Console.ReadLine();
+
+            //bool trueorfalse = 12 > 5;
+            //Console.WriteLine(trueorfalse);
+            //Console.ReadLine();
+
+            int roomTemp = 70;
+            int currentTemp = 70;
+
+            bool isWarm = currentTemp != roomTemp;
+            Console.WriteLine(isWarm);
+            Console.ReadLine();
+        }
+
+    }
+}
