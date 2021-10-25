e603f1c3d67fd37856c935ef71ba7333218e2b18
Austin313 <32691123+Austin313@users.noreply.github.com>
2021-10-25 16:02:22 -0500

    Pushing through for instructor review

---
+++ PackageExpress/PackageExpress/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace PackageExpress
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Package Express! Please Follow the instructions below.");
+            Console.WriteLine("Please enter the Package Weight");
+            string weightString = Console.ReadLine();
+            int Weight = Convert.ToInt32(weightString);
+            if (Weight >= 50)
+            {
+                Console.WriteLine("This package is too heavy. Please reduce the weight.");
+                Console.ReadLine();
+                Environment.Exit(0);
+            }
+            else
+            {
+                Console.WriteLine("Thanks, please enter the dimensions now.");
+            }
+
+            Console.WriteLine("Please enter the Package width:");
+            string widthString = Console.ReadLine();
+            int Width = Convert.ToInt32(widthString);
+
+
+            Console.WriteLine("Please enter the Package height:");
+            string heightString = Console.ReadLine();
+            int Height = Convert.ToInt32(heightString);
+
+            Console.WriteLine("Please enter the Package length:");
+            string LengthString = Console.ReadLine();
+            int Length = Convert.ToInt32(LengthString);
+
+            if (Width + Height + Length > 50)
+            {
+                Console.WriteLine("Package is too big to be shipped via Package Express.");
+                Console.ReadLine();
+                Environment.Exit(0);
+            }
+            int dimensionProd = Width * Length * Height;
+            int totalCost = dimensionProd * Weight / 100;
+
+            Console.WriteLine("Thank you, your qoute is costed at: $" + totalCost);
+            Console.WriteLine("Press enter to close this program...");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ while_dowhile/while_dowhile/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace while_dowhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What's the capital of Texas?");
+            string answer = Console.ReadLine();
+            bool isGuessing = answer == "Austin";
+   
+            while (!isGuessing)
+            {
+                switch (answer)
+                {
+                    case "Austin":
+                        Console.WriteLine("That answer is correct!");
+                        break;
+
+                    default:
+                        Console.WriteLine("That is incorrect, try again.");
+                        break;
+
+                }    
+
+            }
+            //lol is this cheating 
+            Console.WriteLine("That is correct!");
+            Console.ReadLine();
+
+        }
+    }
+}
