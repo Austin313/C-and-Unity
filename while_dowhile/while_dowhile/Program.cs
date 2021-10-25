4d8387edd732dd2108a4b329dbca48480f980d6a
Austin313 <32691123+Austin313@users.noreply.github.com>
2021-10-25 17:39:34 -0500

    adding do while

--- while_dowhile/while_dowhile/Program.cs
+++ while_dowhile/while_dowhile/Program.cs
@@ -6,27 +6,52 @@ namespace while_dowhile
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("What's the capital of Texas?");
-            string answer = Console.ReadLine();
-            bool isGuessing = answer == "Austin";
+            Console.WriteLine("What is 4+4?");
+            int answer = Convert.ToInt32(Console.ReadLine());
+            bool isGuessing = answer == 8;
+            
    
             while (!isGuessing)
-            {
+            { 
                 switch (answer)
                 {
-                    case "Austin":
+                    case 8:
                         Console.WriteLine("That answer is correct!");
+                        isGuessing = true;
                         break;
 
                     default:
                         Console.WriteLine("That is incorrect, try again.");
+                        answer = Convert.ToInt32(Console.ReadLine());
                         break;
 
                 }    
 
             }
-            //lol is this cheating 
-            Console.WriteLine("That is correct!");
+
+            Console.WriteLine("What is 5+6?");
+            int answer2 = Convert.ToInt32(Console.ReadLine());
+            bool isGuessing2 = answer2 == 11;
+
+            do
+            {
+                switch (answer2)
+                {
+                    case 11:
+                        Console.WriteLine("That answer is correct!");
+                        isGuessing2 = true;
+                        break;
+
+                    default:
+                        Console.WriteLine("That is incorrect, try again.");
+                        answer2 = Convert.ToInt32(Console.ReadLine());
+                        break;
+
+                }
+            }
+            while (!isGuessing2);
+            
+
             Console.ReadLine();
 
         }
