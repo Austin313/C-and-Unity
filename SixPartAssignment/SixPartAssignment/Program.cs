e8b15c35f4e2629320540a925a60d262293374ce
Austin313 <32691123+Austin313@users.noreply.github.com>
2021-11-02 14:15:18 -0500

    updated assignment

--- SixPartAssignment/SixPartAssignment/Program.cs
+++ SixPartAssignment/SixPartAssignment/Program.cs
@@ -8,138 +8,115 @@ namespace SixPartAssignment
     {
         static void Main(string[] args)
         {
-            //Part 1
-            List<string> names = new List<string>() { "Austin", "Devin", "Justin", "Nancy" };
-            
-            
-            foreach (string i in names)
+            ////Part 1
+            string[] Students = { "Austin ", "Devin ", "Justin ", "Nancy " };
+            string message = Console.ReadLine();
+            for (int i = 0; i <= 3; i++)
             {
-                Console.WriteLine(i);
+                Console.WriteLine(Students[i] + message);
             }
-            Console.WriteLine("Please add your name to this list!");
-            string name = Console.ReadLine();
-            names.Add(name);
-            Console.WriteLine("Thanks, " + name);
-            Console.WriteLine("/////////////////////////////////////////");
-            foreach (string i in names)
-            {
-                Console.WriteLine(i);
-            }
+            Console.ReadLine();
 
-            int[] infiniteLoop = { 1, 3, 5, 6, 7 };
-            //Part 2
-            //Changing the for statement with i-- would cause the program to continue infinitly into negatives and never reach the value of 5
-            for (int i = 0; i < infiniteLoop.Length; i++)
-            {
-                Console.WriteLine(infiniteLoop[i]);
-            }
-            Console.WriteLine("Wow nice numbers!");
-            int[] damage = { 2, 10, 5, 7, 2 };
-            //Part 3
-            for (int i = 0; i <= damage.Length; i++)
-            {
-                if (i == 1)
-                {
-                    int crit = damage[i] * 2;
-                    Console.WriteLine(crit);
-                    Console.WriteLine("Critical Strike!");
-                }
-                   
-             }
+
+            
+
+            //int[] infiniteLoop = { 1, 3, 5, 6, 7 };
+            ////Part 2
+            ////Changing the for statement with i-- would cause the program to continue infinitly into negatives and never reach the value of 5
+            //for (int i = 0; i < infiniteLoop.Length; i++)
+            //{
+            //    Console.WriteLine(infiniteLoop[i]);
+            //}
+            //Console.WriteLine("Wow nice numbers!");
+            //int[] damage = { 2, 10, 5, 7, 2 };
+            ////Part 3
+            //for (int i = 0; i <= damage.Length; i++)
+            //{
+            //    if (i == 1)
+            //    {
+            //        int crit = damage[i] * 2;
+            //        Console.WriteLine(crit);
+            //        Console.WriteLine("Critical Strike!");
+            //    }
+
+            // }
             //part 4
-            Console.WriteLine("Search a student first name to see their information, or type exit to leave the program");
-            List<string> Students = new List<string>();
-                Students.Add("Nancy");
-                Students.Add("Austin");
-                Students.Add("Rosa");
-                Students.Add("Hank");
-            string search = Convert.ToString(Console.ReadLine());
-            bool exit = false;
+            //Console.WriteLine("Search a student first name to see their information, or type exit to leave the program");
+            //List<string> Students = new List<string>();
+            //    Students.Add("Nancy");
+            //    Students.Add("Austin");
+            //    Students.Add("Rosa");
+            //    Students.Add("Hank");
+            //string search = Console.ReadLine();
+            //bool isPresent = Students.Contains(search);
+            //if (isPresent == true)
+            //{
+            //    int index = Students.FindIndex(x => x.Contains(search));
+            //    Console.WriteLine(index);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Sorry your input is not valid");
+            //}
 
-            do
-            {
-                switch (search)
-                {
-                    case "Nancy":
-                        Console.WriteLine(Students[0]);
-                        Console.WriteLine("PhoneNum:555-555-1313 Email: NancyN@email.com");
-                        search = Convert.ToString(Console.ReadLine());
-                        break;
-                    case "Austin":
-                        Console.WriteLine(Students[1]);
-                        Console.WriteLine("PhoneNum:555-555-1335 Email: AustinN@email.com");
-                        search = Convert.ToString(Console.ReadLine());
-                        break;
-                    case "Rosa":
-                        Console.WriteLine(Students[2]);
-                        Console.WriteLine("PhoneNum:555-555-1374 Email: RosaN@email.com");
-                        search = Convert.ToString(Console.ReadLine());
-                        break;
-                    case "Hank":
-                        Console.WriteLine(Students[3]);
-                        Console.WriteLine("PhoneNum:555-555-1265 Email: HankN@email.com");
-                        search = Convert.ToString(Console.ReadLine());
-                        break;
-                    case "exit":
-                        exit = true;
-                        break;
-                    default:
-                        Console.WriteLine("That is not a valid entry, try again");
-                        search = Convert.ToString(Console.ReadLine());
-                        break;
-                }
-            }
-            while (!exit);
             //part 5
-            Console.WriteLine("Search for a produce item");
-            List<string> produce = new List<string>();
-            produce.Add("Orange");
-            produce.Add("Grape");
-            produce.Add("Orange");
-            produce.Add("Apple");
-            produce.Add("Cherry");
-            Console.WriteLine(produce[0]);
-            Console.WriteLine(produce[1]);
-            Console.WriteLine(produce[2]);
-            Console.WriteLine(produce[3]);
-            Console.WriteLine(produce[4]);
-            foreach (string fruits in produce)
-            {
-                
+            //Console.WriteLine("Search for a produce item");
+            //List<string> produce = new List<string>();
+            //produce.Add("Orange");
+            //produce.Add("Grape");
+            //produce.Add("Orange");
+            //produce.Add("Apple");
+            //produce.Add("Cherry");
+            //string item = Console.ReadLine();
+            //for (int i = 0; i <= (produce.Count - 1); i++)
+            //{
+            //    if (produce[i] == item)
+            //    {
+            //        Console.WriteLine("Item found!: " + i);
 
-                if (fruits.Contains("Orange"))
-                {
-                    string fruit = Convert.ToString(Console.ReadLine());
-                    Console.WriteLine(fruit);
-                }
-            }
-            //part 6
-            List<string> meat = new List<string>();
-            meat.Add("beef");
-            meat.Add("Chicken");
-            meat.Add("beef");
-            meat.Add("pork");
-            foreach (string duplicate in meat)
-            {
-                
-                if (meat.Contains(duplicate))
-                {
-                    string selection = Convert.ToString(Console.ReadLine());
-                    Console.WriteLine(selection);
-                    Console.WriteLine("This item is already added!");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("This item is not on the list");
+            //    }
 
-
-
-                }
-                else
-                {
-                    Console.WriteLine("Item will be added!");
-                    meat.Add(duplicate);
-                }
-
-            }
-
+            //}
+            //List<string> meat = new List<string>();
+            //meat.Add("beef");
+            //meat.Add("chicken");
+            //meat.Add("beef");
+            //meat.Add("Pork");
+            //List<string> search = new List<string>();
+            //foreach (string x in meat)
+            //{
+            //    if (search.Contains(x)) 
+            //    {
+            //        Console.WriteLine("This has been added");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("this item has not been added");
+            //    }
+            //    search.Add(x);
+            //    Console.WriteLine(x);
+            //}
             Console.ReadLine();
         }
     }
 }
+
+
+
+
+
+        
+            
+                
+                
+
+            
+
+            
+        
+    
+
