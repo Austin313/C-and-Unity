347d49ca946e5f19d67dd411fe4037d76f19e732
Austin313 <32691123+Austin313@users.noreply.github.com>
2021-11-03 16:26:55 -0500

    Added calling method assignment

---
+++ classes and objects/classes and objects/Calling_Methods.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classes_and_objects
+{
+    class Calling_Methods
+    {
+        public static void method1()
+        {
+            Console.WriteLine("Lets Multiply!");
+            int op1 = Convert.ToInt32(Console.ReadLine());
+            int op2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(op1 * op2);
+            
+        }
+        public static void method2()
+        {
+            Console.WriteLine("Lets Divide!");
+            int op1 = Convert.ToInt32(Console.ReadLine());
+            int op2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(op1 / op2);
+            
+        }
+        public static void method3()
+        {
+            Console.WriteLine("Lets Add!");
+            int op1 = Convert.ToInt32(Console.ReadLine());
+            int op2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(op1 + op2);
+        }
+
+    }
+}
---
+++ classes and objects/classes and objects/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classes_and_objects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Calling_Methods.method1();
+            Calling_Methods.method2();
+            Calling_Methods.method3();
+            Console.ReadLine();
+            //    deck deck = new deck();
+            //    deck = shuffle(deck);
+
+            //    foreach (card card in deck.cards)
+            //    {
+            //        console.writeline(card.face + " of " + card.suit);
+            //    }
+            //    console.writeline(deck.cards.count);
+            //    console.readline();
+            //}
+
+            //public static deck shuffle(deck deck)
+            //{
+            //    list<card> templist = new list<card>();
+            //    random random = new random();
+
+            //    while (deck.cards.count > 0)
+
+            //    {
+            //        int randomindex = random.next(0, deck.cards.count);
+            //        templist.add(deck.cards[randomindex]);
+            //        deck.cards.removeat(randomindex);
+            //    }
+            //    deck.cards = templist;
+            //    return deck;
+            
+           
+
+
+        }
+
+        
+        
+    }
+}
