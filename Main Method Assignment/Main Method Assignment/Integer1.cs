9c8f841701bbddab6016519cd420216746688d5a
Austin313 <32691123+Austin313@users.noreply.github.com>
2021-11-04 12:55:08 -0500

    pushing

---
+++ Main Method Assignment/Main Method Assignment/Integer1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main_Method_Assignment
+{
+    class Integer1
+    {
+        public static void Method1()
+        {
+            Console.WriteLine("Lets Add!");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            int result = num2 + num1;
+            Console.WriteLine(result);
+        }
+        public static void Method2()
+        {
+            Console.WriteLine("Multiply two decimals together");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            double num2 = Convert.ToDouble(Console.ReadLine());
+            double result = num1 * num2;
+            Console.WriteLine(result);
+        }
+        public static void Method3()
+        {
+            Console.WriteLine("Enter a number");
+            int Number = Convert.ToInt32(Console.ReadLine());
+            int result = Number * 6;
+            Console.WriteLine(Number + " Times 6 equals " + result);
+        }
+    }
+}
---
+++ Main Method Assignment/Main Method Assignment/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main_Method_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Integer1.Method1();
+            Console.ReadLine();
+            Integer1.Method2();
+            Console.ReadLine();
+            Integer1.Method3();
+        }
+    }
+}
