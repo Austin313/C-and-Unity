b0fd927c13c91d92bb17d0e5e2abbf0bb5879186
Austin313 <32691123+Austin313@users.noreply.github.com>
2021-11-12 13:25:51 -0600

    Used an if/else statement to fix the format bug. Now, if user enters a blank value it will auto fill the second parameter

--- Method Submission Assignment/Method Submission Assignment/Program.cs
+++ Method Submission Assignment/Method Submission Assignment/Program.cs
@@ -6,15 +6,34 @@ namespace Method_Submission_Assignment
     {
         static void Main(string[] args)
         {
-            Class1 Class = new Class1();
+            
+            
 
-            Console.WriteLine("Please enter some numbers to add");
-            int num1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("The next number has a default value of 6, no need to enter anything");
-            string num2 = Convert.ToString(Console.ReadLine());
-            int _num2 = Convert.ToInt32(num2);
-            Class.Method(num1,_num2);
-            Console.ReadLine();
+                Class1 Class = new Class1();
+
+                Console.WriteLine("Please enter some numbers to add");
+                int num1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("The next number has a default value of 6, no need to enter anything");
+                string num2 = Console.ReadLine();
+                bool empty = num2 == "";
+
+            if (empty == true)
+                {
+                int _num2 = 6;
+                Class.Method(num1, _num2);
+                Console.ReadLine();
+            }
+            else
+            {
+                int _num2 = Convert.ToInt32(num2);
+                Class.Method(num1, _num2);
+                Console.ReadLine();
+            }
+
+                
+            
+
+            
 
         }
     }
