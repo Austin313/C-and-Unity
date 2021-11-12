916376632669cb16ee42477bd14a2968e7e3c18f
Austin313 <32691123+Austin313@users.noreply.github.com>
2021-11-12 13:58:28 -0600

    Commiting for submission

---
+++ Method Class submission assignment/Method Class submission assignment/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Class_submission_assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Void Void = new Void();
+            
+            
+            Void.Method(1,6);
+            Console.ReadLine();
+            Console.WriteLine("----------------------------");
+            Console.WriteLine("Please enter in some numbers");
+
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("----------------------------");
+            Void.Method(num1, num2);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Method Class submission assignment/Method Class submission assignment/Void.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Class_submission_assignment
+{
+    class Void
+    {
+        public void Method(int num1, int num2)
+        {
+            int operation = num1 * 5;
+           
+            Console.WriteLine(num2);
+        }
+        
+        
+        
+
+
+    }
+}
